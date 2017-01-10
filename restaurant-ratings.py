bbd392773e48e12ca666e6294c1b60b358f391e1
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-09 17:59:14 -0800

    Added update a random rest rating

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,6 +1,7 @@
 # your code goes here
 
 import sys
+import random
 
 
 def create_ratings_dict():
@@ -39,21 +40,38 @@ def print_sorted_reviews(rest_ratings):
     #     print "{} is rated at {}.".format(rest, rating)
 
 
+def update_random_rest_rating(rest_ratings):
+    """Chooses random restaurant and prompts user for new rating."""
+
+    rand_rest = random.choice(sorted(rest_ratings))
+    rand_rating = raw_input("What is your rating for {}? ".format(rand_rest))
+
+    rest_ratings[rand_rest] = rand_rating
+
+
 def perform_user_choice():
     """Prompts user to choose between data visualization, data append, and quit."""
 
     rest_ratings = create_ratings_dict()
     while True:
         print
-        user_choice = raw_input("Press 1 to see all ratings, press 2 add a new rating, press q to quit: ")
+        user_choice = raw_input("Press 1 to see all ratings, press 2 to modify ratings, and press q to quit: ")
         print
         if user_choice == "1":
             print_sorted_reviews(rest_ratings)
         elif user_choice == "2":
-            add_user_rating(rest_ratings)
+            user_choice2 = raw_input("Press 1 to add a new rating, press 2 to update a random rating: ")
+            print
+            if user_choice2 == "1":
+                add_user_rating(rest_ratings)
+            elif user_choice2 == "2":
+                update_random_rest_rating(rest_ratings)
+            else:
+                print "Please press 1 or 2."
         elif user_choice == "q":
             return
         else:
             print "Please press 1, 2, or q."
+
 
 perform_user_choice()
